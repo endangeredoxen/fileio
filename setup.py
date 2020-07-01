be2baef7c5761230d5fb18e8d46e53f7871d7b2f
Steve Nicholes <steve.nicholes@gmail.com>
2020-07-01 10:28:37 -0600

    fix invalid classifiers for pip

--- setup.py
+++ setup.py
@@ -46,11 +46,10 @@ setup(
         'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
-        'Intended Audience :: Engineers/Scientists',
-        'Topic :: Data Analysis',
+        'Intended Audience :: Science/Research',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: GPL v3 License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
